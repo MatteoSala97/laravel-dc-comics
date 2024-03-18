2d94ecde57d509c7e6d4941a193bf7a891569b38
Matteo Sala <matteo111997@gmail.com>
2024-03-18 16:04:44 +0100

    first commit

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Dc Comics | Home')
+
+@section('main')
+    <main>
+        <h2 class="text-success">
+            hello from welcome
+        </h2>
+    </main>
+@endsection
